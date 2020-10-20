4ad7ea3a58a6e52746637bbd4df83728b19d583f
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2020-10-20 18:16:09 +0900

    Call enableProdMode

---
+++ src/renderer/environments/environment.ts
@@ -0,0 +1,4 @@
+
+export const environment = {
+  production: process.env.NODE_ENV === 'production',
+};
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -2,5 +2,11 @@ import './polyfills';
 import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
